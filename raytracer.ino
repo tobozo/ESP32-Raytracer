37fe031c0463f24ccb7385404bb35844e34ea82d
tobozo <tobozo@users.noreply.github.com>
2019-01-22 10:26:09 +0100

    initial import

---
+++ raytracer.ino
@@ -0,0 +1,12 @@
+//#include "geometry.h"
+#include "tinyraytracer.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  arduinomain();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
